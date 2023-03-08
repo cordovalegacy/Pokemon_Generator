c8b9e304367506e92e685837a702ddb578032e16
Brendan Cordova <cordovalegacy19@gmail.com>
2023-03-08 08:17:53 -0800

    cleanup

--- client/src/components/poke_form.js
+++ client/src/components/poke_form.js
@@ -3,13 +3,11 @@ import { useState } from 'react';
 const PokeForm = (props) => {
 
     const { list, setList } = props
-    const [pokeData, setPokeData] = useState({
-        name: ""
-    })
+    const [pokeData, setPokeData] = useState("")
 
     const submitHandler = async (e) => {
         e.preventDefault();
-        const results = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeData.name}`)
+        const results = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeData}`)
         const pokeResults = await results.json()
         console.log(pokeResults)
         setList([...list, pokeResults])
@@ -23,8 +21,8 @@ const PokeForm = (props) => {
                 <input
                     type="text"
                     placeholder='Pick a pokemon!'
-                    value={pokeData.name}
-                    onChange={(e) => setPokeData({ ...pokeData, name: e.target.value })}
+                    value={pokeData}
+                    onChange={(e) => setPokeData(e.target.value)}
                 />
                 <input type="submit" value="Add" />
             </div>
